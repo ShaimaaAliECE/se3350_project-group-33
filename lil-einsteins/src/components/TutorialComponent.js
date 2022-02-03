d38b6ee2b1b715e59e0ab1676ac3c54030304cb6
wsucklal <warrensucklal@gmail.com>
2022-02-03 14:38:06 -0500

    added steps with matching animations and text displays

--- lil-einsteins/src/components/TutorialComponent.js
+++ lil-einsteins/src/components/TutorialComponent.js
@@ -16,19 +16,43 @@ function Tutorial () {
     //     layer5 : {tutorialText: null},
     // };
 
-    const[tutorialText, setTutorialText] = useState("This algorithm works by dividing the full list of values into two equally sized sublists.")
-    const[step, setStep] = useState(1);
-    const[showAnimation, setShowAnimation] = useState([])
+    const[tutorialText, setTutorialText] = useState("You’ve selected the merge sort algorithm.")
+    const[step, setStep] = useState(0);
+    const[showNextAnimation, setShowNextAnimation] = useState(false)
 
     //Layers Enabled
-    const[layer2,  setLayer2] = useState(true);
-    const[layer3,  setLayer3] = useState(true);
-    const[layer4,  setLayer4] = useState(true);
-    const[layer5,  setLayer5] = useState(true);
+    const[layer2,  setLayer2] = useState(false);
+    const[layer3,  setLayer3] = useState(false);
+    const[layer4,  setLayer4] = useState(false);
+    const[layer5,  setLayer5] = useState(false);
     
 
+
     useEffect(() => {
-        
+        switch(step){
+
+            case 0:
+                setTutorialText("You’ve selected the merge sort algorithm.");
+                setLayer2(false);
+                setLayer3(false);
+                setLayer4(false);
+                setLayer5(false);
+                break;
+
+            case 1:
+                setTutorialText("This algorithm works by dividing the full list of values into two equally sized sublists.");
+                setLayer2(true);
+                break;
+                // 
+            case 2:
+                setTutorialText("Then the left sublist is further divided until it only contains one value.")
+                setLayer3(true);
+                setLayer4(true);
+                setLayer5(true);
+                break;
+            // default:
+            //     setLayer2(false);
+        }
     });
 
     
@@ -177,7 +201,7 @@ function Tutorial () {
                     in={layer3}
                     timeout={400}
                     classNames = "numberContainer"
-                    unmountOnExit
+                    unmountOnExit/*when the element disappears, it’s actually going to leave the DOM*/
                     >
                         <ul>
                             <li>
@@ -221,7 +245,7 @@ function Tutorial () {
                     in={layer4}
                     timeout={400}
                     classNames = "numberContainer"
-                    unmountOnExit
+                    unmountOnExit/*when the element disappears, it’s actually going to leave the DOM*/
                     >
 
                         <ul>
@@ -254,7 +278,7 @@ function Tutorial () {
                     in={layer5}
                     timeout={400}
                     classNames = "numberContainer"
-                    unmountOnExit
+                    unmountOnExit/*when the element disappears, it’s actually going to leave the DOM*/
                     >
                         <ul>
                             <li>
@@ -280,6 +304,12 @@ function Tutorial () {
                 </button>
                 <button className="nextBtn" onClick={() => setLayer2(true)}>
                     Next
+                </button>
+                <button className="prevBtn" onClick ={() => setStep(step + 1)}>
+                    Test
+                </button>
+                <button className="prevBtn" onClick ={() => setStep(0)}>
+                    Reset
                 </button>
     
             </div>
