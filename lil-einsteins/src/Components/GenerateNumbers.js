9500f9c55062bc79bfb2fc610ee31854ed3731c0
Amir <keshtkaramirhossein@yahoo.com>
2022-03-31 12:47:03 -0400

    fixed random number function

--- lil-einsteins/src/Components/GenerateNumbers.js
+++ lil-einsteins/src/Components/GenerateNumbers.js
@@ -1,17 +1,17 @@
-const pro_IntDigit = () => Math.floor(Math.random() * 50) + 1;
-const randomizeNewArray = (number) => {
-	let temp = [];
-	for (let i = 0; i < number; i++) {
-		let item = pro_IntDigit();
-		temp.push(item);
-	}
-	const random_val = (temp) => {
-		temp.sort(function (a, b) {
-			return b - a;
-		});
-	};
-	random_val(temp);
-	console.log(temp);
-	return temp;
+const randomizeNewArray = (number, range) => {
+  const pro_IntDigit = () => Math.floor(Math.random() * range) + 1;
+  let temp = [];
+  for (let i = 0; i < number; i++) {
+    let item = pro_IntDigit();
+    temp.push(item);
+  }
+  const random_val = (temp) => {
+    temp.sort(function (a, b) {
+      return b - a;
+    });
+  };
+  random_val(temp);
+  console.log(temp);
+  return temp;
 };
 export default randomizeNewArray;
