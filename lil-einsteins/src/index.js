58b33823f9be555834a626d13fbde520d5cf548e
liamSharkey <wsharke@uwo.ca>
2022-04-01 16:29:59 -0400

    couple small bug fixes

--- lil-einsteins/src/index.js
+++ lil-einsteins/src/index.js
@@ -11,23 +11,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComponent from "./Components/NavbarComponent";
 
 //required change for push
+//second required change for push
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <NavbarComponent /> */}
-    <Router>
-      <Routes className="my-5">
-        <Route path="/Home" element={<Home />}></Route>
-        <Route path="/Level1" element={<Tutorial />}></Route>
-        <Route path="/level2" element={<Level2 />}></Route>
-        <Route path="/level3" element={<Level3 />}></Route>
-        <Route path="/level4" element={<Level4 />}></Route>
-        <Route path="/level5" element={<Level5 />}></Route>
-        <Route path="/" element={<Tutorial />}></Route>
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		{/* <NavbarComponent /> */}
+		<Router>
+			<Routes className="my-5">
+				<Route path="/Home" element={<Home />}></Route>
+				<Route path="/Level1" element={<Tutorial />}></Route>
+				<Route path="/level2" element={<Level2 />}></Route>
+				<Route path="/level3" element={<Level3 />}></Route>
+				<Route path="/level5" element={<Level4 />}></Route>
+				<Route path="/level4" element={<Level5 />}></Route>
+				<Route path="/" element={<Home />}></Route>
+			</Routes>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
