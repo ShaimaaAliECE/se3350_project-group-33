f991c42408644044ee440856d126eff814beeb6e
Amir <keshtkaramirhossein@yahoo.com>
2022-04-01 16:24:09 -0400

    Fixed some bugs

--- lil-einsteins/src/Components/Timer.js
+++ lil-einsteins/src/Components/Timer.js
@@ -1,65 +1,64 @@
-import React, {useCallback} from "react";
+import React, { useCallback } from "react";
 import "../App.css";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Timer = () => {
-	// Clock keeps track of miliseconds
-	// Time sets the current time
-	const [clock, time] = React.useState(0); // Default value for time
+  // Clock keeps track of miliseconds
+  // Time sets the current time
+  const [clock, time] = React.useState(0); // Default value for time
 
-	// Checks if the timer is running
-	const [play, start] = React.useState(false); // False is default value
+  // Checks if the timer is running
+  const [play, start] = React.useState(false); // False is default value
 
-	// Hook runs any time the user presses play and timer is turned on
-	React.useEffect(() => {
-		// Declaring the current interval of the timer is set to null
-		let current = null;
+  // Hook runs any time the user presses play and timer is turned on
+  React.useEffect(() => {
+    // Declaring the current interval of the timer is set to null
+    let current = null;
 
-		// If the timer is turned on by play button
-		if (play) {
-			current = setInterval(() => {
-				time((prevTime) => prevTime + 10); // Every 10 miliseconds the time is increased by 10 miliseconds
-			}, 10); // Increases the time every 10 miliseconds
-		} else if (!play) {
-			// If the player presses the play button then the time is cleared
-			clearInterval(current);
-		}
-	}, [play]);
-	
-	if(clock === 300000){
-       handleLevelChange();
-     }
+    // If the timer is turned on by play button
+    if (play) {
+      current = setInterval(() => {
+        time((prevTime) => prevTime + 10); // Every 10 miliseconds the time is increased by 10 miliseconds
+      }, 10); // Increases the time every 10 miliseconds
+    } else if (!play) {
+      // If the player presses the play button then the time is cleared
+      clearInterval(current);
+    }
+  }, [play]);
 
-     const navigate = useNavigate();
-	const handleLevelChange = useCallback(
-		() => navigate("/Home", {replace: true}),
-		[navigate]
-	);
+  if (clock === 300000) {
+    handleLevelChange();
+  }
 
+  const navigate = useNavigate();
+  const handleLevelChange = useCallback(
+    () => navigate("/Home", { replace: true }),
+    [navigate]
+  );
 
-	return (
-		<div className="Timer mx-3 my-3">
-			<h2>Game Time: </h2>
-			<div id="display">
-				<span>{("0" + Math.floor((clock / 60000) % 60)).slice(-2)}:</span>
-				<span>{("0" + Math.floor((clock / 1000) % 60)).slice(-2)}:</span>
+  return (
+    <div className="Timer mx-3 my-3">
+      <h2>Game Time: </h2>
+      <div id="display">
+        <span>{("0" + Math.floor((clock / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((clock / 1000) % 60)).slice(-2)}:</span>
 
-				<span>{("0" + ((clock / 10) % 100)).slice(-2)}</span>
-			</div>
+        <span>{("0" + ((clock / 10) % 100)).slice(-2)}</span>
+      </div>
 
-			<div id="buttons">
-				{!play && clock === 0 && (
-					// Sets timer to true which starts timer is play button is pressed
-					<button onClick={() => start(true)}>Play</button>
-				)}
-				{play && (
-					// Sets timer to false so turns timer off if game is done
-					<button onClick={() => start(false)}>Done</button>
-				)}
-				{!play && clock > 0 && <button onClick={() => time(0)}></button>}
-			</div>
-		</div>
-	);
+      <div id="buttons">
+        {!play && clock === 0 && (
+          // Sets timer to true which starts timer is play button is pressed
+          <button onClick={() => start(true)}>Play</button>
+        )}
+        {play && (
+          // Sets timer to false so turns timer off if game is done
+          <button onClick={() => start(false)}>Done</button>
+        )}
+        {!play && clock > 0 && <button onClick={() => time(0)}></button>}
+      </div>
+    </div>
+  );
 };
 
 export default Timer;
